1718aba3a5b6479b8ead44f70ede1a0f7d56c75a
Rebecca <rebecca.christine.sanders@gmail.com>
2023-08-31 13:17:19 -0400

    first commit

---
+++ src/main/java/com/portfolio/portfoliors/PortfolioRsApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.portfoliors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioRsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioRsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/portfoliors/PortfolioRsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.portfoliors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioRsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
